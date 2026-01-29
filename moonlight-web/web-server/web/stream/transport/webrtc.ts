94b8cccd2c763f65027e942d69262704308307eb
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2026-01-29 15:10:26 +0100

    fix: do not treat webrtc disconnect as fatal (cherry-pick from upstream 2cb210f)

--- moonlight-web/web-server/web/stream/transport/webrtc.ts
+++ moonlight-web/web-server/web/stream/transport/webrtc.ts
@@ -208,7 +208,7 @@ export class WebRTCTransport implements Transport {
         if (this.peer.connectionState == "connected") {
             type = "recover"
             this.setDelayHintInterval(true)
-        } else if ((this.peer.connectionState == "failed" || this.peer.connectionState == "disconnected") && this.peer.iceGatheringState == "complete") {
+        } else if ((this.peer.connectionState == "failed" || this.peer.connectionState == "closed") && this.peer.iceGatheringState == "complete") {
             type = "fatal"
             this.setDelayHintInterval(false)
         }
